e7939936a9e56f89a0485e88127bc99abc4fec0a
jiazhen xi <jiazhen xi>
2017-05-09 22:48:00 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# crawler-toy
-small toy of crawling techmeme website
+# crawling [Techmeme]
+[Techmeme]: https://www.techmeme.com/
+
+Note:
+Techmeme is mainly a technology news site.(https://www.techmeme.com/)
+
+This is a small toy of crawling Techmeme website.
+Simply doing 2 things:
+- crawling the web data (news titles of one day) from: `https://www.techmeme.com/river`
+- generate a wordcloud based on the news titles.
+
+example image of wordcloud:
+![img](https://github.com/john7farrell/techmmPrj/blob/master/wordcloud.jpg)
---
+++ crl_test.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+@author: john
+
+"""
+#%% crawling data
+
+import requests
+from bs4 import BeautifulSoup
+
+def getSrcHtml(urlToGet):
+    return  requests.get(urlToGet).text
+
+
+url = 'https://www.techmeme.com/river'
+src_html = getSrcHtml(url)
+
+soup = BeautifulSoup(src_html,'lxml')
+river_items_raw = soup.select('tr.ritem > td > a')
+
+def getTitleLink(items_all):
+    data_li = []
+    for item in items_all:
+        title = item.get_text()
+        link = item.get('href')
+        data = {
+                'title': title,
+                'link': link,
+                }
+        print(data)
+        data_li.append(data)
+    return data_li
+
+river_items_li = getTitleLink(river_items_raw)
+
+#%% generate wordcloud
+
+items_all = [d['title'] for d in river_items_li]
+itemsStr = ' '.join(items_all).lower()
+
+import matplotlib.pyplot as plt
+import wordcloud
+from nltk.corpus import stopwords
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+stopwords_addition= {'say', 'says', 'year', 'will', 'new', 'source', 'sources'}
+STOPWORDS_ = STOPWORDS.union(stopwords_addition)
+wc = WordCloud(background_color="white", 
+               width=1280, height=720,
+               stopwords=STOPWORDS_, 
+               max_font_size=80,
+               min_font_size=10
+               )
+wc.generate(itemsStr)
+
+wc.to_image()
+wc.to_file('./wordcloud.jpg')
